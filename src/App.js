9c87174cd1aecb41a2cf0d9ece80e8ebfc2b3144
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2023-10-24 15:05:41 +0500

    Changes in component folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div>
-      
+      <Header />
+      <HeroSection />
+      <Footer />
     </div>
   );
 }
